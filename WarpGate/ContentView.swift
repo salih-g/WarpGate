b1642a220197a4cad6a7fcff337a35eec63a984f
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2024-03-01 01:31:17 +0300

    Initial Commit

---
+++ WarpGate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarpGate
+//
+//  Created by Muhammed Salih Gümüş on 1.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarpGate/WarpGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarpGateApp.swift
+//  WarpGate
+//
+//  Created by Muhammed Salih Gümüş on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WarpGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
