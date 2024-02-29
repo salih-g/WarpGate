8f173809d04b7a4a49c91167ecac51626feb6e95
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2024-03-01 02:13:17 +0300

    chore: update icons

--- WarpGate/WarpGateApp.swift
+++ WarpGate/WarpGateApp.swift
@@ -7,12 +7,23 @@
 
 import SwiftUI
 
+
 @main
 struct WarpGateApp: App {
     var body: some Scene {
-      MenuBarExtra("UtilityApp", systemImage: "hammer") {
+      MenuBarExtra {
             AppMenu()
-        }.menuBarExtraStyle(.window)
+        } label: {
+            let image: NSImage = {
+                let ratio = $0.size.height / $0.size.width
+                $0.size.height = 18
+                $0.size.width = 18 / ratio
+                return $0
+            }(NSImage(named: "warp_gate")!)
+
+            Image(nsImage: image)
+        }
+      .menuBarExtraStyle(.window)
         
         // WindowGroup {
         //     ContentView()
